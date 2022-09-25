df133159e3043568c7baf00a517f515aedfa88b9
Andrew T <atenni@users.noreply.github.com>
2022-09-25 20:34:33 +1000

    feat: add optional effort and priority values

--- create-task-in-height.ts
+++ create-task-in-height.ts
@@ -7,33 +7,61 @@
 
 // Optional parameters:
 // @raycast.icon ✅
-// @raycast.argument1 { "type": "text", "placeholder": "task" }
+// @raycast.argument1 { "type": "text", "placeholder": "name" }
+// @raycast.argument2 { "type": "text", "placeholder": "effort", "optional": true }
+// @raycast.argument3 { "type": "text", "placeholder": "priority", "optional": true }
 
 // Documentation:
 // @raycast.author @atenni
 // @raycast.authorURL https://github.com/atenni/raycast-scripts
+// @raycast.description Create a todo in the Height app, with an optional effort and priority.
 
 import { config as loadConfig } from "https://deno.land/std@0.157.0/dotenv/mod.ts";
 
 const config = await loadConfig();
 
 async function main() {
-  const task = Deno.args[0];
+  const [name, effort, priority] = Deno.args; // `effort` and `priority` are empty strings if not provided
+  let feedback = "Success!";
+
+  // Create fields array
+  const fields = [];
+
+  // Map effort to its UUID
+  if (["1", "2", "3"].includes(effort)) {
+    fields.push({
+      fieldTemplateId: config["HEIGHT_EFFORT_FIELD_TEMPLATE_UUID"],
+      value: config[`HEIGHT_EFFORT_${effort}_UUID`],
+    });
+  } else if (effort) {
+    feedback = `${feedback} Effort ignored - must be 1, 2, or 3.`;
+  }
+
+  // Map priority to its UUID
+  if (["1", "2", "3"].includes(priority)) {
+    fields.push({
+      fieldTemplateId: config["HEIGHT_PRIORITY_FIELD_TEMPLATE_UUID"],
+      value: config[`HEIGHT_PRIORITY_${priority}_UUID`],
+    });
+  } else if (priority) {
+    feedback = `${feedback} Priority ignored - must be 1, 2, or 3.`;
+  }
 
   // Add the task to the default list
-  const res = await fetch(`${config["HEIGHT_BASE_URL"]}/tasks`, {
+  await fetch(`${config["HEIGHT_BASE_URL"]}/tasks`, {
     method: "POST",
     headers: {
       "Authorization": `api-key ${config["HEIGHT_SECRET_KEY"]}`,
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      name: task,
+      name,
       listIds: [config["HEIGHT_DEFAULT_LIST_UUID"]],
+      fields,
     }),
   });
 
-  console.log(await res.json());
+  console.log(feedback);
 }
 
 main().catch((err) => {
